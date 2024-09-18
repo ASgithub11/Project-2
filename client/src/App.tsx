746ece6cde348a53b8b617bcc51bcc6784c091d4
Keith <keith.amadeus.williams@gmail.com>
2024-09-17 21:55:28 -0600

    added logic for RandomPhoto.tsx and App.tsx, will address errors

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RandomPhoto from './RandomPhoto';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Random Photo from Unsplash</h1>
+      <RandomPhoto />
+    </div>
+  );
+}
+
+export default App;
