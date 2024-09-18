c8d3a4ba041c911c7a7505f3f04903efad716676
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-09-17 22:18:17 -0600

    rsvp route be

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { userRouter } from './user-route.js';
 import { eventRouter } from './event-route.js';
+import { rsvpRouter } from './RSVP-route.js';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/events', eventRouter);
+router.use('./rsvps', rsvpRouter);
 
 export default router;
