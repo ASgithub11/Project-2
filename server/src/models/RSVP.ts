d5a6748259f598aa4bba082ffa1b04e014ad5796
Ash <aishasiddiqa151@gmail.com>
2024-09-19 17:21:02 -0700

    Add RSVP model which extends the class from Sequelize

--- server/src/models/RSVP.ts
+++ server/src/models/RSVP.ts
@@ -7,3 +7,16 @@ interface RSVPAttributes {
   eventId: number;  // Event the RSVP is for
   status: string;   // RSVP status: 'yes', 'no', or 'maybe'
 }
+
+// Optional id attribute for creating a new RSVP
+interface RSVPCreationAttributes extends Optional<RSVPAttributes, 'id'> {}
+
+export class RSVP extends Model<RSVPAttributes, RSVPCreationAttributes> implements RSVPAttributes {
+  public id!: number;
+  public userId!: number;
+  public eventId!: number;
+  public status!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
