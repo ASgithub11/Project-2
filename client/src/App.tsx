d47de6c0e065d99b8bf7c56948f3e55e15ca8c25
Ash <aishasiddiqa151@gmail.com>
2024-09-22 01:23:49 -0700

    Import all the pages and Navbar component, add Route path to all the pages in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
-import { Outlet } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-function App() {
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import Events from './pages/Events';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Navbar from './components/Navbar';
+import CreateEvent from './pages/CreateEvent';
+
+const App = () => {
   return (
     <div>
-      {/* <Navbar /> */}
-      <main className='container pt-5'>
-        <Outlet />
-      </main>
-    </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/create-event" element={<CreateEvent />} />
+      </Routes>
+    </div>  
   );
-}
+};
+
 export default App;
