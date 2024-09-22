e9750746f3589f42be51a407002446c6ed993ed4
Ash <aishasiddiqa151@gmail.com>
2024-09-22 00:57:57 -0700

    Update Navbar.tsx and create AboutUs, CreateEvent, Events, Login and Signup pages

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,15 +2,18 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import './Navbar.css';
+import './Navbar.css';
 
 const Navbar: React.FC = () => {
     return (
         <nav className="navbar">
+            <div className="logo">YourLogo</div>    {/* We can add Logo of our website here */}
             <ul className='nav-bar-links'>
                 <li><Link to="/">Home</Link></li>   {/* Link to the Home page */}
                 <li><Link to="/events">Events</Link></li>   {/* Link to the Events page */}
-                <li><Link to="/login">Login/Signup</Link></li>   {/* Link to Login/Signup page */}
+                <li><Link to="/about">About Us</Link></li>   {/* Link to the About Us page */}
+                <li><Link to="/login">Login</Link></li>   {/* Link to Login page */}
+                <li><Link to="/signup">Signup</Link></li>   {/* Link to Signup page */}
             </ul>
         </nav>
     );
