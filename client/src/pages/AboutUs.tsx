e54532de4485bb0cb13acb7023ce46f493d82db9
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-09-24 17:59:31 -0600

    about us style

--- client/src/pages/AboutUs.tsx
+++ client/src/pages/AboutUs.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import './AboutUs.css';
+import aboutus1 from '../assets/images/aboutus1.png';
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div className="about-container">
+      <header className="hero"><h1>About Us</h1>
       <p>SocialSync is a platform for community-driven events. Our goal is to help people connect with local activities and initiatives.</p>
+      </header>
+     <div className="mission"> <img src={aboutus1} alt="a sign that says good vibes only"/>
+     <div className="mission-text">
       <h2>Our Mission</h2>
       <p>We believe in building stronger communities through engagement, collaboration, and shared experiences.</p>
       {/* Add additional information here */}
+      </div>
+      </div>
     </div>
   );
 };
