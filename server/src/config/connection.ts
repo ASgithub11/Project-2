4d15b01a81681f33446931fc84b0aced3b8ee838
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-09-24 18:44:38 -0600

    reverting last commit

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,23 +3,42 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
-let sequelize;
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME || '',
+      process.env.DB_USER || '',
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME || '',
-    process.env.DB_USER || '',
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    }
-  )
-}
+// the code below is what the docs said to use but it kept giving us errors
+
+// let sequelize;
+
+// if (process.env.DB_URL) {
+//   sequelize = new Sequelize(process.env.DB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME || '',
+//     process.env.DB_USER || '',
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     }
+//   )
+// }
+
+
 
 export default sequelize;
