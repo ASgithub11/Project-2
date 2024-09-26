39d8b2db2dcd9a2ad996fe20b1452de5b38334a3
Keith <keith.amadeus.williams@gmail.com>
2024-09-26 12:33:05 -0600

    Connect to postgresql

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,22 +1,22 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-import { Sequelize } from 'sequelize';
+// import { Sequelize } from 'sequelize';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(
+//       process.env.DB_NAME || '',
+//       process.env.DB_USER || '',
+//       process.env.DB_PASSWORD,
+//       {
+//         host: 'localhost',
+//         dialect: 'postgres',
+//         dialectOptions: {
+//           decimalNumbers: true,
+//         },
+//       }
+//     );
 
 // the code below is what the docs said to use but it kept giving us errors
 
@@ -40,5 +40,27 @@ const sequelize = process.env.DB_URL
 // }
 
 
+import { Sequelize } from 'sequelize';
+
+let sequelize: Sequelize = new Sequelize(process.env.DATABASE_URL || '', {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+});
+
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME || '',
+    process.env.DB_USER || '',
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
 
 export default sequelize;
