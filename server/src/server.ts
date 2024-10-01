6c870af5c6b8c9f11afda94052acddc86d6476d9
Ash <aishasiddiqa151@gmail.com>
2024-09-30 22:13:22 -0700

    Changed package.json file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
-const forceDatabaseRefresh = false;
+// const forceDatabaseRefresh = false;
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+// import { Sequelize } from 'sequelize';
 import sequelize from './config/connection.js';
 import routes from './routes/index.js';
 import authRoutes from './routes/auth-routes.js';
@@ -12,6 +13,7 @@ import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// const port = process.env.PORT || 3000;
 
 app.use(cors());
 
@@ -25,8 +27,14 @@ app.use(routes);
 // Add the event router for the /api/events route
 app.use('/api/events', eventRouter);
 
-sequelize.sync({force: forceDatabaseRefresh}).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err: Error) => {
+    console.error('Unable to connect to the database:', err);
   });
-});
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+  });
